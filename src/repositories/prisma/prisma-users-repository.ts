190f86149d6bdd99050dae9f36c05d808b7b65aa
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-18 18:50:48 +0000

    Design Patterns & Testes - Primeiro teste unitário

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -12,6 +12,7 @@ export class PrismausersRepository implements UsersRepository {
 
     return user;
   }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
