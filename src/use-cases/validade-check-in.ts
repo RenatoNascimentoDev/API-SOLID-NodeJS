0f4d5eaea4738e07e2d44bf5c9a57b9c30aed24f
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-27 16:13:34 +0000

    Implementando casos de uso - Caso de uso de validar de check-in

---
+++ src/use-cases/validade-check-in.ts
@@ -0,0 +1,32 @@
+import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { CheckIn } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface ValidadeCheckInUseCaseRequest {
+  checkInId: string;
+}
+
+interface ValidadeCheckInUseCaseResponse {
+  checkIn: CheckIn;
+}
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidadeCheckInUseCaseRequest): Promise<ValidadeCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId);
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError();
+    }
+
+    checkIn.validated_at = new Date();
+
+    await this.checkInsRepository.save(checkIn);
+
+    return {
+      checkIn,
+    };
+  }
+}
